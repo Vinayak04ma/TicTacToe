9a3dff7ad50ceec9821d08bcfdb3750aa30065f9
vinayak04ma <vinayakmaheshwari57@gmail.com>
2025-08-06 19:05:44 +0530

    Initial commit 123

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from './app/Store.js'
+// import { store } from './app/Store.js'
 import { Provider } from 'react-redux'
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <Provider >
     <App />
   </Provider>
 )
